44e6a0e94473afef8f85d91ee6338c5fe5c1d86f
Tanmay Singh Thakur <itsthakurtanmay@gmail.com>
2025-05-25 20:06:06 +0530

    Mern stack tutorial

---
+++ JavaScript/Lecture 1/first.js
@@ -0,0 +1,22 @@
+console.log("Hello javscript"); //We'll used console.log for printing purpose in js
+
+//abhi starting mein output nahi aayega starting mein file run nahi hogi.
+
+//Question 1 : Ye jo Javascript haii ye toh console.log samjh rha haii but VS code kyu nahi samjh rha?
+//Answer 1 : Because google ke pass V8 engine haii.
+
+            //jaise  c++ ka code haii usko run krwane ke liye compiler chahiye hota haii naa jaise gcc/trubo c++ waise hi isko bhi run karna hai toh iske pass bhi kuch hona chahiye , toh google ke pass toh V8 engine hai but Vs code toh humara simple txt editor haii uske pass aisa kuch nahi haii jisse vo code samjh paaye !
+
+
+
+//number ko store karna haii;
+let num = 10;
+console.log(num);//agar hum kisi or language se aate haii C++/Java toh DT btna pdta hai ki int haii char haiii yha koi need nahi haii btane ki;
+
+let naam = "Hello Js data types";
+console.log(naam);
+
+const id = 20;
+console.log(id);
+//id = 35;
+//it will give you type error :  values can't be changed
---
+++ JavaScript/Lecture 2/first.js
@@ -0,0 +1,46 @@
+// DATA TYPES IN JAVASCRIPT -> Primitive Data Type & Non Primitive Data Types;
+//Primitive Data Types -> Number , boolean , string , null , undefined , Symbol , bigint
+let account_balanace = 20;
+console.log(account_balanace);
+
+
+//Q. How to confirm ki ye account_balance ka data type number type ka hi haii;
+//Ans : We have typeof -> ye account_balance ka type of bta dega yaani kis type ki value iske andar rakhi gyi haii
+console.log(typeof account_balanace); 
+
+
+//string;
+let str = "tanmay is revising Js";
+console.log(str);
+console.log(typeof str);
+
+//Note : Semicolon is allowed or not allowed there is not strict rule for this;
+
+let statement  = true;
+console.log(statement);
+
+console.log(typeof statement);
+
+
+//Undefined;
+let account;//jab humne iske andar koi value nahi rkhi ho , tab undefined aata haii;
+console.log(account);
+console.log(typeof account);
+
+//Null;
+let bal = null;
+console.log(bal);
+console.log(typeof bal);
+
+//bigint = boht bda number;
+let bignum = 345678902345675678656787668n;
+console.log(bignum);
+console.log(typeof bignum);
+
+
+
+
+
+
+//Non Primitive Data Types -> Array , Object , Function
+
